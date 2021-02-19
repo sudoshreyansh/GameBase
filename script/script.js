636898218981fad4d8be9947d4e754fc9a47aecd
sudoshreyansh <shreyanshjain14072002@gmail.com>
2021-02-19 14:25:02 +0530

    Created mobile slider

--- script/script.js
+++ script/script.js
@@ -4,4 +4,25 @@ document.querySelector('.hamburger').addEventListener('click', function() {
 
 window.addEventListener('scroll', () => {
     document.querySelector('header').classList.toggle('sticky', window.scrollY > 0);
-})
+});
+
+document.querySelectorAll('.games-slider').forEach(element => {
+    let currentPos = 0;
+    let gameCardsWrapper = element.querySelector('.games-slider__cards');
+    let gameCards = element.querySelectorAll('.games-slider__card');
+    let cardWidth = gameCards[0].offsetWidth;
+    let trackWidth = gameCardsWrapper.scrollWidth;
+    let scrollWidth = trackWidth - element.offsetWidth;
+
+    element.querySelector('.games-slider__button-left').addEventListener('click', () => {
+        let newPos = currentPos - cardWidth;
+        currentPos = newPos < 0 ? 0 : newPos;
+        gameCardsWrapper.style.transform = `translateX(-${currentPos}px)`;
+    });
+
+    element.querySelector('.games-slider__button-right').addEventListener('click', () => {
+        let newPos = currentPos + cardWidth;
+        currentPos = newPos > scrollWidth ? scrollWidth : newPos;
+        gameCardsWrapper.style.transform = `translateX(-${currentPos}px)`;
+    });
+});
