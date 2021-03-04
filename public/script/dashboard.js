3cb298f5a4ea531e6ce5fb39dfeb6bd158e10fa5
sudoshreyansh <shreyanshjain14072002@gmail.com>
2021-03-05 00:18:02 +0530

    Added dashboard load code

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,3 +1,15 @@
-function authCallback(user) {
-    console.log(user);
+function displayProfile() {
+    document.getElementById('profile').classList.add('loaded');
+}
+
+function setProfile(username) {
+    document.getElementById('profile__username').innerText = `@${username}`;
+    displayProfile();
+}
+
+async function authCallback(user) {
+    let uid = user.uid;
+    let documentSnapshot = await getFromDatabase(usersCollection.doc(uid));
+    let userData = documentSnapshot.data();
+    setProfile(userData.username);
 }
